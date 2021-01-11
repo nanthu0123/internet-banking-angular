03f8eb6f6545c46f1764f8d4a0f006c36aadadf8
nanthu0123 <nanthu0123@gmail.com>
2021-01-11 12:07:08 +0530

    login register page

--- src/app/user-pages/register/register.component.ts
+++ src/app/user-pages/register/register.component.ts
@@ -11,5 +11,56 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
+  url_adharFront: String | ArrayBuffer = '';
+  url_adharBack: String | ArrayBuffer = '';
+  url_pan: String | ArrayBuffer = '';
+  url_photo: String | ArrayBuffer = '';
 
+  onSelectAdharFront(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url_adharFront = event.target.result;
+      }
+    }
+  }
+
+  onSelectAdharBack(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => {
+        this.url_adharBack = event.target.result;
+      }
+    }
+  }
+
+  onSelectPan(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => {
+        this.url_pan = event.target.result;
+      }
+    }
+  }
+
+  onSelectPhoto(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => {
+        this.url_photo = event.target.result;
+      }
+    }
+  }
 }
