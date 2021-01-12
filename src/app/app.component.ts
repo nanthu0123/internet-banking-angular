d58d515d5f01665aef369090f1bc0a0eaf307033
nanthu0123 <nanthu0123@gmail.com>
2021-01-12 19:45:09 +0530

    content -wrapper padding removed in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
           document.querySelector('.page-body-wrapper').classList.add('full-page-wrapper');
           document.querySelector('.content-wrapper').classList.remove('auth', 'auth-img-bg',);
           document.querySelector('.content-wrapper').classList.remove('auth', 'lock-full-bg');
-          if ((event['url'] == '/error-pages/404') || (event['url'] == '/error-pages/500')) {
+          if ((event['url'] == '/error-pages/404') || (event['url'] == '/error-pages/500') || (event['url'] == '/user-pages/login')) {
             document.querySelector('.content-wrapper').classList.add('p-0');
           }
         } else {
