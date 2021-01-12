de7fb7e9265aaf0c22d570d41fb57e52a33ac645
nanthu0123 <nanthu0123@gmail.com>
2021-01-12 17:54:26 +0530

    loan status page created

--- src/app/loan/loan.module.ts
+++ src/app/loan/loan.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ApplyNewLoanComponent } from './apply-new-loan/apply-new-loan.component';
 import { ViewAllLoanComponent } from './view-all-loan/view-all-loan.component';
+import { LoanStatusComponent } from './loan-status/loan-status.component';
 
 
 const routes: Routes = [
   { path: 'apply-new-loan', component: ApplyNewLoanComponent },
   { path: 'view-all-loan', component: ViewAllLoanComponent },
+  { path: 'loan-status', component: LoanStatusComponent },
 
 ]
 
 @NgModule({
-  declarations: [ApplyNewLoanComponent, ViewAllLoanComponent,],
+  declarations: [ApplyNewLoanComponent, ViewAllLoanComponent, LoanStatusComponent,],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
