04245eb77fc323e60245371e0fbdc8440866720e
nanthu0123 <nanthu0123@gmail.com>
2021-01-15 16:21:51 +0530

    value added services

---
+++ src/app/value-added-service/other-facilities/other-facilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-other-facilities',
+  templateUrl: './other-facilities.component.html',
+  styleUrls: ['./other-facilities.component.scss']
+})
+export class OtherFacilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
