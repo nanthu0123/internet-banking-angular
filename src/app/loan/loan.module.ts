652028ca6a42c4b6a9878e79dbf5db9f37462c99
nanthu0123 <nanthu0123@gmail.com>
2021-01-20 18:04:06 +0530

    new loan schemes

--- src/app/loan/loan.module.ts
+++ src/app/loan/loan.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { ApplyNewLoanComponent } from './apply-new-loan/apply-new-loan.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatCardModule } from '@angular/material/card';
+
 import { ViewAllLoanComponent } from './view-all-loan/view-all-loan.component';
 import { LoanStatusComponent } from './loan-status/loan-status.component';
+import { ApplyLoanComponent } from './apply-loan/apply-loan.component';
+import { PersonalLoanComponent } from './apply-loan/personal-loan/personal-loan.component';
+import { HomeLoanComponent } from './apply-loan/home-loan/home-loan.component';
+import { BusinessLoanComponent } from './apply-loan/business-loan/business-loan.component';
 
 
 const routes: Routes = [
-  { path: 'apply-new-loan', component: ApplyNewLoanComponent },
+  { path: 'apply-loan', component: ApplyLoanComponent },
   { path: 'view-all-loan', component: ViewAllLoanComponent },
   { path: 'loan-status', component: LoanStatusComponent },
+  { path: 'apply-loan/personal-loan', component: PersonalLoanComponent },
+  { path: 'apply-loan/home-loan', component: HomeLoanComponent },
+  { path: 'apply-loan/business-loan', component: BusinessLoanComponent },
 
-]
+
+];
 
 @NgModule({
-  declarations: [ApplyNewLoanComponent, ViewAllLoanComponent, LoanStatusComponent,],
+  declarations: [ViewAllLoanComponent, LoanStatusComponent, ApplyLoanComponent, PersonalLoanComponent, HomeLoanComponent, BusinessLoanComponent,],
   imports: [
     CommonModule,
+    NgbModule,
+    MatCardModule,
     RouterModule.forChild(routes),
   ]
 })
