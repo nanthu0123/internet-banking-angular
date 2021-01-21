d77738127fd8b663fe3c92ba33c2ed2ea2ad46a3
nanthu0123 <nanthu0123@gmail.com>
2021-01-21 15:25:38 +0530

    loan status removed

--- src/app/loan/loan.module.ts
+++ src/app/loan/loan.module.ts
@@ -5,7 +5,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatCardModule } from '@angular/material/card';
 
 import { ViewAllLoanComponent } from './view-all-loan/view-all-loan.component';
-import { LoanStatusComponent } from './loan-status/loan-status.component';
 import { ApplyLoanComponent } from './apply-loan/apply-loan.component';
 import { PersonalLoanComponent } from './apply-loan/personal-loan/personal-loan.component';
 import { HomeLoanComponent } from './apply-loan/home-loan/home-loan.component';
@@ -15,7 +14,6 @@ import { BusinessLoanComponent } from './apply-loan/business-loan/business-loan.
 const routes: Routes = [
   { path: 'apply-loan', component: ApplyLoanComponent },
   { path: 'view-all-loan', component: ViewAllLoanComponent },
-  { path: 'loan-status', component: LoanStatusComponent },
   { path: 'apply-loan/personal-loan', component: PersonalLoanComponent },
   { path: 'apply-loan/home-loan', component: HomeLoanComponent },
   { path: 'apply-loan/business-loan', component: BusinessLoanComponent },
@@ -24,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [ViewAllLoanComponent, LoanStatusComponent, ApplyLoanComponent, PersonalLoanComponent, HomeLoanComponent, BusinessLoanComponent,],
+  declarations: [ViewAllLoanComponent, ApplyLoanComponent, PersonalLoanComponent, HomeLoanComponent, BusinessLoanComponent,],
   imports: [
     CommonModule,
     NgbModule,
