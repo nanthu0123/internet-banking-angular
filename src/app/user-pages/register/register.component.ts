70419b936130443675270a12a6d4280068ff7f7e
nanthu0123 <nanthu0123@gmail.com>
2021-01-27 23:08:49 +0530

    reg page update

--- src/app/user-pages/register/register.component.ts
+++ src/app/user-pages/register/register.component.ts
@@ -6,17 +6,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  country: Boolean = true;
+  RegForm: Boolean = false;
+  docPreview: Boolean = false;
+  accountType: Boolean = false;
+  // register page members
+  aadharFrontName: string;
+  aadharBackName: string;
+  panName: string;
+  photoName: string;
 
-  constructor() { }
+  depositOptions: any[] = [
+    { name: 'Current Account' },
+    { name: 'Savings Account' },
+    { name: 'Fixed Deposit Account' },
+    { name: 'Recurring Deposit Account' },
+  ];
+  selected: String = "Choose Option";
+  schemeCard: Boolean = false;
+  notification: Boolean = false;
+
+  constructor() { };
 
   ngOnInit() {
   }
+  onCountryCard() {
+    this.country = false;
+    this.RegForm = true;
+  }
+  onRegister() {
+    this.RegForm = false;
+    this.docPreview = true;
+  }
+  onDocPreview() {
+    this.docPreview = false;
+    this.accountType = true;
+  }
+  selectOption(event: any) {
+    //getted from event
+    console.log(event.target.value);
+    return event.target.value;
+
+  }
+  onSchemeOptions() {
+    this.accountType = false;
+    this.schemeCard = true;
+
+  }
+  onNotification() {
+    this.notification = true;
+    this.schemeCard = false;
+  }
+
+
+
   url_adharFront: String | ArrayBuffer = '';
   url_adharBack: String | ArrayBuffer = '';
   url_pan: String | ArrayBuffer = '';
   url_photo: String | ArrayBuffer = '';
 
   onSelectAdharFront(event) {
+    this.aadharFrontName = event.target.value.replace(/^.*[\\\/]/, ''); //get file name from absolute path
+
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
 
@@ -24,11 +75,12 @@ export class RegisterComponent implements OnInit {
 
       reader.onload = (event) => { // called once readAsDataURL is completed
         this.url_adharFront = event.target.result;
-      }
+      };
     }
   }
 
   onSelectAdharBack(event) {
+    this.aadharBackName = event.target.value.replace(/^.*[\\\/]/, '');
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
 
@@ -36,11 +88,12 @@ export class RegisterComponent implements OnInit {
 
       reader.onload = (event) => {
         this.url_adharBack = event.target.result;
-      }
+      };
     }
   }
 
   onSelectPan(event) {
+    this.panName = event.target.value.replace(/^.*[\\\/]/, '');
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
 
@@ -48,11 +101,12 @@ export class RegisterComponent implements OnInit {
 
       reader.onload = (event) => {
         this.url_pan = event.target.result;
-      }
+      };
     }
   }
 
   onSelectPhoto(event) {
+    this.photoName = event.target.value.replace(/^.*[\\\/]/, '');
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
 
@@ -60,7 +114,7 @@ export class RegisterComponent implements OnInit {
 
       reader.onload = (event) => {
         this.url_photo = event.target.result;
-      }
+      };
     }
   }
 }
